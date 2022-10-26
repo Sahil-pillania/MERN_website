681ff77a28b2969b7d76c059b99e225d0abe8d99
Sahil pillania <sahilpillania88@gmail.com>
2022-10-26 21:58:09 +0530

    mongodb atlas connection established

--- server/app.js
+++ server/app.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const mongoose = require("mongoose");
 
-app.get("/", (req, res) => {
+const PORT = 5000;
+const DB =
+  "mongodb+srv://sahil:Sahil1234@cluster0.cxi2awi.mongodb.net/mernwebsite?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("mongo connected successfully");
+  })
+  .catch((err) => {
+    console.log("Error occured during connecting to database - " + err);
+  });
+// middleware
+const middleware = (req, res, next) => {
+  console.log("Hello this is middleware");
+  next();
+};
+
+app.get("/", middleware, (req, res) => {
   res.send("Hello world");
 });
 
