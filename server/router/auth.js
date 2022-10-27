377f8c46533b79e270fb1d10e4d818d388f5402a
Sahil pillania <sahilpillania88@gmail.com>
2022-10-27 12:22:36 +0530

    Saving the data into User collection using promises

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,13 +1,44 @@
 const express = require("express");
 const router = express.Router();
+require("../db/conn");
+const User = require("../model/userSchema");
 
 router.get("/", (req, res) => {
   res.send("Hello world from router ");
 });
 
 router.post("/register", (req, res) => {
-  console.log(req.body);
-  res.json({ message: req.body });
+  const { name, email, phone, work, password, cpassword } = req.body;
+
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res
+      .status(422)
+      .json({ error: "please fill all the fields properly" });
+  }
+
+  User.findOne({ email: email })
+    .then((exists) => {
+      if (exists) {
+        return res.status(422).json({ error: "Email already exists." });
+      }
+
+      const user = new User({ name, email, phone, work, password, cpassword });
+
+      user
+        .save()
+        .then(() => {
+          return res
+            .status(201)
+            .json({ message: "User registered succesfully." });
+        })
+        .catch((err) => {
+          return res.status(500).json({ error: "Failed to be registered." });
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   //res.send("register page");
 });
 
