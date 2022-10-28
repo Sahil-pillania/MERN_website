b260f8a0424f30f76fb179257da7cd92a1a5fdd8
Sahil pillania <sahilpillania88@gmail.com>
2022-10-28 20:38:18 +0530

    updated comments

--- server/app.js
+++ server/app.js
@@ -13,14 +13,14 @@ app.use(require("./router/auth")); // router file to make our links
 const PORT = process.env.PORT;
 
 // middleware
-const middleware = (req, res, next) => {
-  console.log("Hello this is middleware");
-  next();
-};
+// const middleware = (req, res, next) => {
+//   console.log("Hello this is middleware");
+//   next();
+// };
 
-app.get("/", middleware, (req, res) => {
-  res.send("Hello world");
-});
+// app.get("/", middleware, (req, res) => {
+//   res.send("Hello world");
+// });
 
 app.listen("5000", () => {
   console.log(`Backend App is running in port no ${PORT}.`);
