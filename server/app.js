65f49a58fbd71a83f80b2be8b555288ed818b9d2
Sahil pillania <sahilpillania88@gmail.com>
2022-10-29 22:35:32 +0530

    db connection updates

--- server/app.js
+++ server/app.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
 //const mongoose = require("mongoose");
+const connectDB = require("./db/conn");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
-require("./db/conn");
 app.use(express.json());
 
 // const User = require("./model/userSchema");
 app.use(require("./router/auth")); // router file to make our links
 
 const PORT = process.env.PORT;
-
+connectDB();
 // middleware
 // const middleware = (req, res, next) => {
 //   console.log("Hello this is middleware");
